0a5dc81663a61af23a6f1d5489f35fc0fb8cc9d3
Warren Alphonso <warrenalphonso02@gmail.com>
2020-11-14 14:18:27 -0800

    Added another page to test links

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import Link from "next/link"
 
 const Header = (): JSX.Element => {
   return (
     <div>
-      <h1>Warren</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <br />
+      <Link href="/courses">
+        <a>Courses</a>
+      </Link>
     </div>
   )
 }

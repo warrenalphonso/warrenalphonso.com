3fba6e8b742c9066af16789dde0cf3317deae45d
Warren Alphonso <warrenalphonso02@gmail.com>
2020-11-13 22:29:29 -0800

    Converted to TS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-// import '../styles/globals.css'
+import { AppProps } from 'next/app'
+
 import "../styles/tailwind.css"
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 

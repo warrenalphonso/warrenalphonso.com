ade3dc6c45f4c5a5a9956efaa0c27e31ba7fb45b
Warren Alphonso <warrenalphonso02@gmail.com>
2020-12-31 16:20:54 -0800

    Chose color scheme for clusters, automated CSS via nth-of-type

---
+++ pages/clusters.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Head from "next/head"
+
+const Clusters = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Clusters | Warren Alphonso</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Clusters</h1>
+
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+        <div className="cluster">
+          <p className="font-extrabold text-lg m-0">Favorites</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Clusters

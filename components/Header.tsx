31e409bd4b5bb07cc12cce73cfc87e864d8ca82f
Warren Alphonso <warrenalphonso02@gmail.com>
2021-03-10 22:50:02 -0800

    Just add link-n to <navbar> so Tailwind doesn't 'optimize' them out of CSS

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header = (): JSX.Element => {
   return (
     <nav className="navbar">
       <Link href="/">
-        <a className="navlinks">Home</a>
+        <a className="navlinks link-zero link-one link-two link-three link-four link-five">Home</a>
       </Link>
       <Link href="/courses">
         <a className="navlinks">Courses</a>

5dd96a6e35db3367fa1bb5d0d57945116dab451e
Warren Alphonso <warrenalphonso02@gmail.com>
2021-12-11 15:34:33 -0800

    Tailwind v3.0.0 changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
